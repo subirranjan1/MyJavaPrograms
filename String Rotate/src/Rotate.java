68c59087946712467562c23529319824d63b4112
subirranjan1 <subir.ranjan1@gmail.com>
2015-04-28 02:19:08 -0700

    String Rotate Program

---
+++ String Rotate/src/Rotate.java
@@ -0,0 +1,29 @@
+/**
+ * 
+ */
+
+/**
+ * @author Subir
+ *
+ */
+public class Rotate {
+
+	/**
+	 * @param args
+	 */
+	int A=10;
+	
+	public void Print_Rotate(int A)
+	{
+		System.out.println(A);
+		
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Rotate obj=new Rotate();
+		System.out.println("A");
+		obj.Print_Rotate(10);
+	}
+
+}
